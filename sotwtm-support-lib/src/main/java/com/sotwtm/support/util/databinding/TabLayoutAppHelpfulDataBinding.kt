a144a939b7609b860e2b6a55c7194f40b4c21f14
John Tsai <sheungon@gmail.com>
2018-04-11 20:30:05 +0800

    Updated TabLayout binding methods.

--- sotwtm-support-lib/src/main/java/com/sotwtm/support/util/databinding/TabLayoutAppHelpfulDataBinding.kt
+++ sotwtm-support-lib/src/main/java/com/sotwtm/support/util/databinding/TabLayoutAppHelpfulDataBinding.kt
@@ -1,8 +1,9 @@
 package com.sotwtm.support.util.databinding
 
-import android.databinding.BindingMethod
-import android.databinding.BindingMethods
+import android.databinding.*
+import android.databinding.adapters.ListenerUtil
 import android.support.design.widget.TabLayout
+
 
 /**
  * DataBinding methods and BindingMethods created for easier implementation for Android DataBinding.
@@ -13,4 +14,52 @@ import android.support.design.widget.TabLayout
 @BindingMethods(
         BindingMethod(type = TabLayout::class, attribute = "setupWithViewPager", method = "setupWithViewPager")
 )
-object TabLayoutAppHelpfulDataBinding
+object TabLayoutAppHelpfulDataBinding {
+
+    @JvmStatic
+    @BindingAdapter("selectTabAt")
+    fun selectTab(view: TabLayout,
+                  selectedTab: Int) {
+        view.getTabAt(selectedTab)?.select()
+    }
+
+    @Synchronized
+    @JvmStatic
+    @BindingAdapter(value = ["onTabSelected", "selectTabChanged"],
+            requireAll = false)
+    fun setTabChangeListener(view: TabLayout,
+                             listener: TabLayout.OnTabSelectedListener?,
+                             inverseBindingListener: InverseBindingListener?) {
+        val oldListener: TabLayout.OnTabSelectedListener? = ListenerUtil.getListener(view, view.id)
+        if (oldListener != null) {
+            view.removeOnTabSelectedListener(oldListener)
+        }
+        if (inverseBindingListener != null) {
+            val listenerWrapper = object : TabLayout.OnTabSelectedListener {
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    inverseBindingListener.onChange()
+                    listener?.onTabReselected(tab)
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    listener?.onTabUnselected(tab)
+                }
+
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    inverseBindingListener.onChange()
+                    listener?.onTabSelected(tab)
+                }
+
+            }
+            ListenerUtil.trackListener(view, listenerWrapper, view.id)
+            view.addOnTabSelectedListener(listenerWrapper)
+        } else if (listener != null) {
+            ListenerUtil.trackListener(view, listener, view.id)
+            view.addOnTabSelectedListener(listener)
+        }
+    }
+
+    @JvmStatic
+    @InverseBindingAdapter(attribute = "selectTabAt", event = "selectTabChanged")
+    fun getScrollY(view: TabLayout): Int = view.selectedTabPosition
+}
