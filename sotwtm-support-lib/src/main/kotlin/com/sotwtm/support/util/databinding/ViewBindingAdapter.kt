0822b461e027c26ea1c57056fc7abdc55d7c5b1f
John Tsai <sheungon@gmail.com>
2018-12-01 20:38:22 +0800

    Added binding methods for showing snackbar message.

--- sotwtm-support-lib/src/main/kotlin/com/sotwtm/support/util/databinding/ViewBindingAdapter.kt
+++ sotwtm-support-lib/src/main/kotlin/com/sotwtm/support/util/databinding/ViewBindingAdapter.kt
@@ -2,18 +2,37 @@ package com.sotwtm.support.util.databinding
 
 import android.databinding.BindingAdapter
 import android.support.annotation.DrawableRes
+import android.support.design.widget.Snackbar
 import android.view.View
+import com.sotwtm.support.util.SnackbarUtil
 
 object ViewBindingAdapter {
 
     @JvmStatic
     @BindingAdapter("backgroundResource")
-    fun setBackgroundRes(view: View,
-                         @DrawableRes resId: Int?) {
+    fun setBackgroundRes(
+        view: View,
+        @DrawableRes resId: Int?
+    ) {
         if (resId == null) {
             view.background = null
         } else {
             view.setBackgroundResource(resId)
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter(
+        value = ["snackbarMsg",
+            "snackbarDuration"],
+        requireAll = false
+    )
+    fun showSnackbar(
+        view: View,
+        msg: String?,
+        duration: Int?
+    ) {
+        if (msg == null) return
+        SnackbarUtil.make(view, msg, duration ?: Snackbar.LENGTH_LONG)
     }
 }
