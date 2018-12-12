17e1dcc0c55cdcf80c27ff941662c1f33b66ed8a
John Tsai <sheungon@gmail.com>
2018-12-12 20:26:10 +0800

    Added helper class for observing SharedPreferences.

---
+++ sotwtm-support-lib/src/main/kotlin/com/sotwtm/support/rx/ObservableSharedPreferencesBoolean.kt
@@ -0,0 +1,52 @@
+package com.sotwtm.support.rx
+
+import android.content.SharedPreferences
+import android.databinding.ObservableBoolean
+
+class ObservableSharedPreferencesBoolean(
+    private val sharedPreferences: SharedPreferences,
+    private val editor: SharedPreferences.Editor,
+    private val preferenceKey: String,
+    private val defaultValue: Boolean
+) : ObservableBoolean() {
+
+    private val sharedPreferencesListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (key == preferenceKey) {
+                notifyChange()
+            }
+        }
+    private var registeredListener = true
+
+    init {
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
+
+    @Synchronized
+    fun restartPrefListener() {
+        if (registeredListener) return
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = true
+    }
+
+    @Synchronized
+    fun endPrefListener() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = false
+    }
+
+    @Synchronized
+    override fun get(): Boolean =
+        sharedPreferences.getBoolean(preferenceKey, defaultValue)
+
+    @Synchronized
+    override fun set(value: Boolean) {
+        if (get() == value) {
+            return
+        }
+
+        editor.putBoolean(preferenceKey, value)
+        editor.apply()
+        notifyChange()
+    }
+}
---
+++ sotwtm-support-lib/src/main/kotlin/com/sotwtm/support/rx/ObservableSharedPreferencesString.kt
@@ -0,0 +1,52 @@
+package com.sotwtm.support.rx
+
+import android.content.SharedPreferences
+import android.databinding.ObservableField
+
+class ObservableSharedPreferencesString (
+    private val sharedPreferences: SharedPreferences,
+    private val editor: SharedPreferences.Editor,
+    private val preferenceKey: String,
+    private val defaultValue: String
+): ObservableField<String>() {
+
+    private val sharedPreferencesListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (key == preferenceKey) {
+                notifyChange()
+            }
+        }
+    private var registeredListener = true
+
+    init {
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
+
+    @Synchronized
+    fun restartPrefListener() {
+        if (registeredListener) return
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = true
+    }
+
+    @Synchronized
+    fun endPrefListener() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = false
+    }
+
+    @Synchronized
+    override fun get(): String =
+        sharedPreferences.getString(preferenceKey, defaultValue) ?: defaultValue
+
+    @Synchronized
+    override fun set(value: String) {
+        if (get() == value) {
+            return
+        }
+
+        editor.putString(preferenceKey, value)
+        editor.apply()
+        notifyChange()
+    }
+}
