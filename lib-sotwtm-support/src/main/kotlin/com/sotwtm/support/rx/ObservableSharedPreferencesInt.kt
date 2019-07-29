e09f598580745b31830836e45483b9a0a063fe7e
John Tsai <sheungon@gmail.com>
2019-07-29 20:12:46 +0800

    Added ObservableSharedPreferencesInt

---
+++ lib-sotwtm-support/src/main/kotlin/com/sotwtm/support/rx/ObservableSharedPreferencesInt.kt
@@ -0,0 +1,56 @@
+package com.sotwtm.support.rx
+
+import android.content.SharedPreferences
+import android.databinding.ObservableInt
+
+/**
+ * For observing [Int] value in [SharedPreferences]
+ * @author sheungon
+ * */
+class ObservableSharedPreferencesInt(
+    private val sharedPreferences: SharedPreferences,
+    private val editor: SharedPreferences.Editor,
+    private val preferenceKey: String,
+    private val defaultValue: Int
+) : ObservableInt() {
+
+    private val sharedPreferencesListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (key == preferenceKey) {
+                notifyChange()
+            }
+        }
+    private var registeredListener = true
+
+    init {
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
+
+    @Synchronized
+    fun restartPrefListener() {
+        if (registeredListener) return
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = true
+    }
+
+    @Synchronized
+    fun endPrefListener() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener)
+        registeredListener = false
+    }
+
+    @Synchronized
+    override fun get(): Int =
+        sharedPreferences.getInt(preferenceKey, defaultValue)
+
+    @Synchronized
+    override fun set(value: Int) {
+        if (get() == value) {
+            return
+        }
+
+        editor.putInt(preferenceKey, value)
+        editor.apply()
+        notifyChange()
+    }
+}
