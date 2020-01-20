0b16a3f09cab68df90c3d9db781a6a7a32d7ad34
John Tsai <sheungon@gmail.com>
2020-01-20 12:41:28 +0800

    Added service scope.

---
+++ lib-sotwtm-support/src/main/kotlin/com/sotwtm/support/scope/ServiceScope.kt
@@ -0,0 +1,12 @@
+package com.sotwtm.support.scope
+
+import javax.inject.Scope
+
+/**
+ * Service scope
+ * @author sheungon
+ */
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ServiceScope
